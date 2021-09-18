3c3f9054638fa17bc1f13c2a3aa40a3e6ba89a4c
Its[Encrypted] <63492966+makarhuyaker@users.noreply.github.com>
2021-09-18 14:48:56 +0300

    я заебался

--- LauncherAPI/src/main/java/ru/gravit/launcher/request/update/ProfilesRequest.java
+++ LauncherAPI/src/main/java/ru/gravit/launcher/request/update/ProfilesRequest.java
@@ -42,7 +42,7 @@ public final class ProfilesRequest extends Request<ProfilesRequestEvent> impleme
     protected ProfilesRequestEvent requestDo(HInput input, HOutput output) throws Exception {
         output.writeBoolean(true);
         output.flush();
-        readError(input);
+        //readError(input);
 
         int count = 4242;
         //  input.readLength(0)
