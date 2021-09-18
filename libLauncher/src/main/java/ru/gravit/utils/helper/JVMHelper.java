af01116b9fa7feb32bb6252c7f4a7c5765d21cd6
dimadencep <gagarkin.dima2008@gmail.com>
2021-09-18 16:59:02 +0700

    Port JVMHelper from my launcher

--- libLauncher/src/main/java/ru/gravit/utils/helper/JVMHelper.java
+++ libLauncher/src/main/java/ru/gravit/utils/helper/JVMHelper.java
@@ -1,185 +1,193 @@
 package ru.gravit.utils.helper;
 
-import ru.gravit.launcher.LauncherAPI;
-
-import java.io.File;
 import java.lang.invoke.MethodHandles;
 import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-import java.lang.management.RuntimeMXBean;
+import java.util.Locale;
+import java.util.Collection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.File;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Locale;
 import java.util.Map;
+import ru.gravit.launcher.LauncherAPI;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
 
-public final class JVMHelper {
+public final class JVMHelper
+{
+    public static final RuntimeMXBean RUNTIME_MXBEAN;
+    public static final OperatingSystemMXBean OPERATING_SYSTEM_MXBEAN;
     @LauncherAPI
-    public enum OS {
-        MUSTDIE("mustdie"), LINUX("linux"), MACOSX("macosx");
-
-        public static OS byName(String name) {
-            if (name.startsWith("Windows"))
-                return MUSTDIE;
-            if (name.startsWith("Linux"))
-                return LINUX;
-            if (name.startsWith("Mac OS X"))
-                return MACOSX;
-            throw new RuntimeException(String.format("This shit is not yet supported: '%s'", name));
-        }
-
-        public final String name;
-
-        OS(String name) {
-            this.name = name;
-        }
-    }
-
-    // MXBeans exports
-    public static final RuntimeMXBean RUNTIME_MXBEAN = ManagementFactory.getRuntimeMXBean();
-
-    public static final OperatingSystemMXBean OPERATING_SYSTEM_MXBEAN =
-            ManagementFactory.getOperatingSystemMXBean();
-    // System properties
+    public static final OS OS_TYPE;
     @LauncherAPI
-    public static final OS OS_TYPE = OS.byName(OPERATING_SYSTEM_MXBEAN.getName());
+    public static final String OS_VERSION;
     @LauncherAPI
-    public static final String OS_VERSION = OPERATING_SYSTEM_MXBEAN.getVersion();
+    public static final int OS_BITS;
     @LauncherAPI
-    public static final int OS_BITS = getCorrectOSArch();
+    public static final int JVM_BITS;
     @LauncherAPI
-    public static final int JVM_BITS = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+    public static final int RAM;
     @LauncherAPI
-    public static final int RAM = getRAMAmount();
-
-    @LauncherAPI
-    public static final SecurityManager SECURITY_MANAGER = System.getSecurityManager();
-    // Public static fields
-    public static final Runtime RUNTIME = Runtime.getRuntime();
-
-    public static final ClassLoader LOADER = ClassLoader.getSystemClassLoader();
-
-    static {
-        try {
-            MethodHandles.publicLookup(); // Just to initialize class
-        } catch (Throwable exc) {
-            throw new InternalError(exc);
-        }
-    }
-
-
-    public static void appendVars(ProcessBuilder builder, Map<String, String> vars) {
+    public static final SecurityManager SECURITY_MANAGER;
+    public static final Runtime RUNTIME;
+    public static final ClassLoader LOADER;
+    
+    public static void appendVars(final ProcessBuilder builder, final Map<String, String> vars) {
         builder.environment().putAll(vars);
     }
-
-    public static Class<?> firstClass(String... names) throws ClassNotFoundException {
-        for (String name : names)
+    
+    public static Class<?> firstClass(final String... names) throws ClassNotFoundException {
+        final int length = names.length;
+        int i = 0;
+        while (i < length) {
+            final String name = names[i];
             try {
-                return Class.forName(name, false, LOADER);
-            } catch (ClassNotFoundException ignored) {
-                // Expected
+                return Class.forName(name, false, JVMHelper.LOADER);
             }
+            catch (ClassNotFoundException ex) {
+                ++i;
+                continue;
+            }
+            break;
+        }
         throw new ClassNotFoundException(Arrays.toString(names));
     }
-
+    
     @LauncherAPI
     public static void fullGC() {
-        RUNTIME.gc();
-        RUNTIME.runFinalization();
-        LogHelper.debug("Used heap: %d MiB", RUNTIME.totalMemory() - RUNTIME.freeMemory() >> 20);
+        JVMHelper.RUNTIME.gc();
+        JVMHelper.RUNTIME.runFinalization();
+        LogHelper.debug("Used heap: %d MiB", JVMHelper.RUNTIME.totalMemory() - JVMHelper.RUNTIME.freeMemory() >> 20);
     }
-
-
+    
     public static String[] getClassPath() {
         return System.getProperty("java.class.path").split(File.pathSeparator);
     }
-
-
+    
     public static URL[] getClassPathURL() {
-        String[] cp = System.getProperty("java.class.path").split(File.pathSeparator);
-        URL[] list = new URL[cp.length];
-
-        for (int i = 0; i < cp.length; i++) {
+        final String[] cp = System.getProperty("java.class.path").split(File.pathSeparator);
+        final URL[] list = new URL[cp.length];
+        for (int i = 0; i < cp.length; ++i) {
             URL url = null;
             try {
                 url = new URL(cp[i]);
-            } catch (MalformedURLException e) {
+            }
+            catch (MalformedURLException e) {
                 e.printStackTrace();
             }
             list[i] = url;
         }
         return list;
     }
-
-    public static void checkStackTrace(Class<?> mainClass) {
+    
+    public static void checkStackTrace(final Class<?> mainClass) {
         LogHelper.debug("Testing stacktrace");
-        Exception e = new Exception("Testing stacktrace");
-        StackTraceElement[] list = e.getStackTrace();
+        final Exception e = new Exception("Testing stacktrace");
+        final StackTraceElement[] list = e.getStackTrace();
         if (!list[list.length - 1].getClassName().equals(mainClass.getName())) {
             throw new SecurityException(String.format("Invalid StackTraceElement: %s", list[list.length - 1].getClassName()));
         }
     }
-
+    
     private static int getCorrectOSArch() {
-        // As always, mustdie must die
-        if (OS_TYPE == OS.MUSTDIE)
-            return System.getenv("ProgramFiles(x86)") == null ? 32 : 64;
-
-        // Or trust system property (maybe incorrect)
+        if (JVMHelper.OS_TYPE == OS.MUSTDIE) {
+            return (System.getenv("ProgramFiles(x86)") == null) ? 32 : 64;
+        }
         return System.getProperty("os.arch").contains("64") ? 64 : 32;
     }
-
+    
     @LauncherAPI
-    public static String getEnvPropertyCaseSensitive(String name) {
+    public static String getEnvPropertyCaseSensitive(final String name) {
         return System.getenv().get(name);
     }
-
+    
     private static int getRAMAmount() {
-        // TODO Normal fix.
-        int physicalRam = (int) (((com.sun.management.OperatingSystemMXBean) OPERATING_SYSTEM_MXBEAN).getTotalPhysicalMemorySize() >> 20);
-        return Math.min(physicalRam, OS_BITS == 32 ? 1536 : 32768); // Limit 32-bit OS to 1536 MiB, and 64-bit OS to 32768 MiB (because it's enough)
+        final int physicalRam = (int)(((com.sun.management.OperatingSystemMXBean)JVMHelper.OPERATING_SYSTEM_MXBEAN).getTotalPhysicalMemorySize() >> 20);
+        return Math.min(physicalRam, (JVMHelper.OS_BITS == 32) ? 1536 : 32768);
     }
-
+    
     @LauncherAPI
     public static boolean isJVMMatchesSystemArch() {
-        return JVM_BITS == OS_BITS;
+        return JVMHelper.JVM_BITS == JVMHelper.OS_BITS;
     }
-
+    
     @LauncherAPI
-    public static String jvmProperty(String name, String value) {
+    public static String jvmProperty(final String name, final String value) {
         return String.format("-D%s=%s", name, value);
     }
-
+    
     @LauncherAPI
-    public static String systemToJvmProperty(String name) {
+    public static String systemToJvmProperty(final String name) {
         return String.format("-D%s=%s", name, System.getProperties().getProperty(name));
     }
-
+    
     @LauncherAPI
-    public static void addSystemPropertyToArgs(Collection<String> args, String name)
-    {
-        String property = System.getProperty(name);
-        if(property != null)
+    public static void addSystemPropertyToArgs(final Collection<String> args, final String name) {
+        final String property = System.getProperty(name);
+        if (property != null) {
             args.add(String.format("-D%s=%s", name, property));
+        }
     }
-
-
-    public static void verifySystemProperties(Class<?> mainClass, boolean requireSystem) {
+    
+    public static void verifySystemProperties(final Class<?> mainClass, final boolean requireSystem) {
         Locale.setDefault(Locale.US);
-        // Verify class loader
         LogHelper.debug("Verifying class loader");
-        if (requireSystem && !mainClass.getClassLoader().equals(LOADER))
+        if (requireSystem && !mainClass.getClassLoader().equals(JVMHelper.LOADER)) {
             throw new SecurityException("ClassLoader should be system");
-
-        // Verify system and java architecture
+        }
         LogHelper.debug("Verifying JVM architecture");
         if (!isJVMMatchesSystemArch()) {
             LogHelper.warning("Java and OS architecture mismatch");
-            LogHelper.warning("It's recommended to download %d-bit JRE", OS_BITS);
+            LogHelper.warning("It's recommended to download %d-bit JRE", JVMHelper.OS_BITS);
         }
     }
-
+    
     private JVMHelper() {
+    }
+    
+    static {
+        RUNTIME_MXBEAN = ManagementFactory.getRuntimeMXBean();
+        OPERATING_SYSTEM_MXBEAN = ManagementFactory.getOperatingSystemMXBean();
+        OS_TYPE = OS.byName(JVMHelper.OPERATING_SYSTEM_MXBEAN.getName());
+        OS_VERSION = JVMHelper.OPERATING_SYSTEM_MXBEAN.getVersion();
+        OS_BITS = getCorrectOSArch();
+        JVM_BITS = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+        RAM = getRAMAmount();
+        SECURITY_MANAGER = System.getSecurityManager();
+        RUNTIME = Runtime.getRuntime();
+        LOADER = ClassLoader.getSystemClassLoader();
+        try {
+            MethodHandles.publicLookup();
+        }
+        catch (Throwable exc) {
+            throw new InternalError(exc);
+        }
+    }
+    
+    @LauncherAPI
+    public enum OS
+    {
+        MUSTDIE("mustdie"), 
+        LINUX("linux"), 
+        MACOSX("macosx");
+        
+        public final String name;
+        
+        public static OS byName(final String name) {
+            if (name.startsWith("Windows")) {
+                return OS.MUSTDIE;
+            }
+            if (name.startsWith("Linux")) {
+                return OS.LINUX;
+            }
+            if (name.startsWith("Mac OS X")) {
+                return OS.MACOSX;
+            }
+            throw new RuntimeException(String.format("This shit is not yet supported: '%s'", name));
+        }
+        
+        private OS(final String name) {
+            this.name = name;
+        }
     }
 }
