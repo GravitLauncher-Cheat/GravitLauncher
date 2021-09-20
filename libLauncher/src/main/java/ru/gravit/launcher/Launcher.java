011c8acb5a60f1c6abdba17e1404c431d9cb24fb
ItsMakar <cobchenko@gmail.com>
2021-09-20 14:48:44 +0300

    дима не сноси пж это тестовое я потом снесу

--- libLauncher/src/main/java/ru/gravit/launcher/Launcher.java
+++ libLauncher/src/main/java/ru/gravit/launcher/Launcher.java
@@ -40,16 +40,29 @@ public final class Launcher
     public static final Version.Type RELEASE;
     public static GsonBuilder gsonBuilder;
     public static Gson gson;
+    private int configMode;
+    private int runtimeMode;
 
     @LauncherAPI
     public static LauncherConfig getConfig() {
         LauncherConfig config = Launcher.CONFIG.get();
+        JSONParser parser = new JSONParser();
+        JSONObject data = (JSONObject) parser.parse(new FileReader(IOHelper.getCodeSource(Launcher.class).getParent().resolve("config.json").toString()));
+        this.configMode = ((int) data.get("configMode")).intValue();
         if (config == null) {
             try {
-                Path zipfile = Paths.get(IOHelper.getCodeSource(Launcher.class).getParent().resolve("launcher-original.jar").toUri());
-                FileSystem fs = FileSystems.newFileSystem(zipfile, null);
-                final HInput input = new HInput(IOHelper.newInput(fs.getPath("/config.bin")));
-                config = new LauncherConfig(input);
+                if (configMode == 0)
+                {
+                   Path zipfile = Paths.get(IOHelper.getCodeSource(Launcher.class).getParent().resolve("launcher-original.jar").toUri());
+                   FileSystem fs = FileSystems.newFileSystem(zipfile, null);
+                   final HInput input = new HInput(IOHelper.newInput(fs.getPath("/config.bin")));
+                   config = new LauncherConfig(input);
+                }
+                else
+                {
+                    final HInput input = new HInput(IOHelper.newInput(IOHelper.getCodeSource(Launcher.class).getParent().resolve("config.bin")));
+                    config = new LauncherConfig(input);
+                }
             }
             catch (IOException | InvalidKeySpecException ex2) {
                 final Exception e = ex2;
@@ -67,14 +80,24 @@ public final class Launcher
     
     @LauncherAPI
     public static URL getResourceURL(final String name) throws IOException {
+        JSONParser parser = new JSONParser();
+        JSONObject data = (JSONObject) parser.parse(new FileReader(IOHelper.getCodeSource(Launcher.class).getParent().resolve("config.json").toString()));
+        this.runtimeMode = ((int) data.get("runtimeMode")).intValue();
         final LauncherConfig config = getConfig();
         final byte[] validDigest = config.runtime.get(name);
-        if (validDigest == null) {
-            throw new NoSuchFileException(name);
+        //if (validDigest == null) {
+            //throw new NoSuchFileException(name);
+        //}
+        if (runtimeMode == 0)
+        {
+           Path zipfile = Paths.get(IOHelper.getCodeSource(Launcher.class).getParent().resolve("launcher-original.jar").toUri());
+           FileSystem fs = FileSystems.newFileSystem(zipfile, null);
+           final URL url = fs.getPath("runtime/" + name).toUri().toURL();
         }
-        Path zipfile = Paths.get(IOHelper.getCodeSource(Launcher.class).getParent().resolve("launcher-original.jar").toUri());
-        FileSystem fs = FileSystems.newFileSystem(zipfile, null);
-        final URL url = fs.getPath("runtime/" + name).toUri().toURL();
+        else
+        {
+            final URL url = IOHelper.getResourceURL("runtime/" + name);
+        }
         return url;
     }
     
